40e98f5b0100dc6ef54b82037c2fc9d73824098c
andytsai <andytsai2000@gmail.com>
2021-11-25 13:07:02 +0800

    fix

--- main.go
+++ main.go
@@ -8,9 +8,9 @@ import (
 )
 
 type Book struct {
-	Id    string `json:"id"`
-	Name  string `json:"name"`
-	Pages string `json:"pages"`
+	Id    string `json:"Id"`
+	Name  string `json:"Name"`
+	Pages string `json:"Pages"`
 }
 
 var bookshelf = []Book{
@@ -27,9 +27,9 @@ func getBooks(c *gin.Context) {
 }
 
 func getBook(c *gin.Context) {
-	id := c.Param("id")
+	Id := c.Param("Id")
 	for i := range bookshelf {
-		if bookshelf[i].id == id {
+		if bookshelf[i].Id == Id {
 			c.IndentedJSON(http.StatusOK, bookshelf[i])
 			return
 		}
@@ -45,9 +45,9 @@ func addBook(c *gin.Context) {
 }
 
 func deleteBook(c *gin.Context) {
-	id := c.Param("id")
+	Id := c.Param("Id")
 	for i := range bookshelf {
-		if bookshelf[i].id == id {
+		if bookshelf[i].Id == Id {
 			c.IndentedJSON(http.StatusOK, bookshelf[i])
 			bookshelf = append(bookshelf[:i], bookshelf[i+1:]...)
 			return
@@ -58,10 +58,10 @@ func deleteBook(c *gin.Context) {
 
 func updateBook(c *gin.Context) {
 	var book Book
-	id := c.Param("id")
+	Id := c.Param("Id")
 	c.BindJSON(&book)
 	for i := range bookshelf {
-		if bookshelf[i].id == id {
+		if bookshelf[i].Id == Id {
 			bookshelf[i] = book
 			c.IndentedJSON(http.StatusOK, bookshelf[i])
 			return
@@ -74,10 +74,10 @@ func main() {
 	r := gin.Default()
 	r.RedirectFixedPath = true
 	r.GET("/bookshelf", getBooks)
-	r.GET("/bookshelf/:id", getBook)
+	r.GET("/bookshelf/:Id", getBook)
 	r.POST("/bookshelf", addBook)
-	r.DELETE("/bookshelf/:id", deleteBook)
-	r.PUT("/bookshelf/:id", updateBook)
+	r.DELETE("/bookshelf/:Id", deleteBook)
+	r.PUT("/bookshelf/:Id", updateBook)
 
 	port := "8080"
 	if v := os.Getenv("PORT"); len(v) > 0 {
