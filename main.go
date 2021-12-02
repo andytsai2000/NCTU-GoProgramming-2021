1cb25ddde9b52dc3caf077ac99ecaa4bf7c68ae8
andytsai <andytsai2000@gmail.com>
2021-12-02 16:13:08 +0800

    45: index -> id

--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"database/sql"
 	"log"
+	"net/http"
 	"os"
-	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -12,47 +12,45 @@ import (
 )
 
 type Book struct {
-	// write your own struct
-	// id's type is int
-	ID    int    `json:"id"`
+	Id    int    `json:"id"`
 	Name  string `json:"name"`
 	Pages string `json:"pages"`
 }
 
 func getBooks(db *sql.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// [TODO] get all books data
+		/* [TODO] get all books data */
 		rows, _ := db.Query("SELECT * FROM bookshelf")
 
-		// [TODO] scan the data one by one
+		/* [TODO] scan the data one by one */
+		bookshelf := []Book{}
 		defer rows.Close()
-		var book Book
-		var bookList []Book
 		for rows.Next() {
-			rows.Scan(&book.ID, &book.Name, &book.Pages)
-			bookList = append(bookList, book)
+			book := Book{}
+			rows.Scan(&book.Id, &book.Name, &book.Pages)
+			bookshelf = append(bookshelf, book)
 		}
 
 		//[TODO]send all data or error handling
-		if len(bookList) > 0 {
-			c.IndentedJSON(200, bookList)
-		} else {
-			c.IndentedJSON(404, gin.H{"message": "book not found"})
+		if len(bookshelf) != 0 {
+			c.IndentedJSON(http.StatusOK, bookshelf)
+			return
 		}
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "book not found"})
 	}
 }
+
 func getBook(db *sql.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
+		index := c.Param("id")
+		book := Book{}
+		err := db.QueryRow("SELECT * FROM bookshelf WHERE id=$1", index).Scan(&book.Id, &book.Name, &book.Pages)
 
-		n, _ := strconv.Atoi(c.Param("id"))
-		rows := db.QueryRow("SELECT * FROM bookshelf WHERE id=$1", n)
-		var book Book
-		rows.Scan(&book.ID, &book.Name, &book.Pages)
-		if book.ID != 0 {
-			c.IndentedJSON(200, book)
-		} else {
-			c.IndentedJSON(404, gin.H{"message": "book not found"})
+		if err == nil {
+			c.IndentedJSON(http.StatusOK, book)
+			return
 		}
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "book not found"})
 	}
 }
 
@@ -60,42 +58,36 @@ func addBook(db *sql.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		var book Book
 		c.BindJSON(&book)
-		db.QueryRow("INSERT INTO bookshelf VALUES (DEFAULT,$1,$2) RETURNING id", book.Name, book.Pages).Scan(&book.ID)
-		c.IndentedJSON(200, book)
+		db.QueryRow("INSERT INTO bookshelf VALUES (DEFAULT,$1,$2) RETURNING id", book.Name, book.Pages).Scan(&book.Id)
+		c.IndentedJSON(http.StatusOK, book)
 	}
 }
 
 func updateBook(db *sql.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		n, _ := strconv.Atoi(c.Param("id"))
-		var book, oldBook Book
+		var book Book
 		c.BindJSON(&book)
-		rows := db.QueryRow("SELECT * FROM bookshelf WHERE id=$1", n)
-		rows.Scan(&oldBook.ID, &oldBook.Name, &oldBook.Pages)
 
-		//		db.QueryRow("UPDATE bookshelf SET name=$1, pages=$2 WHERE id=$3", book.Name, book.Pages, n).Scan(&book.ID)
-		if oldBook.ID != 0 {
-			db.QueryRow("UPDATE bookshelf SET name=$1, pages=$2 WHERE id=$3", book.Name, book.Pages, n)
-			book.ID = n
-			c.IndentedJSON(200, book)
-		} else {
-			c.IndentedJSON(404, gin.H{"message": "book not found"})
+		err := db.QueryRow("UPDATE bookshelf SET name=$1, pages=$2 WHERE id=$3 RETRUNING *", book.Name, book.Pages, book.Id).Scan()
+		if err == nil {
+			c.IndentedJSON(http.StatusOK, book)
+			return
 		}
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "book not found"})
 	}
 }
 
 func deleteBook(db *sql.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		n, _ := strconv.Atoi(c.Param("id"))
 		var book Book
-		rows := db.QueryRow("SELECT * FROM bookshelf WHERE id=$1", n)
-		rows.Scan(&book.ID, &book.Name, &book.Pages)
-		if book.ID != 0 {
-			c.IndentedJSON(200, book)
-			db.Query("DELETE FROM bookshelf WHERE id=$1 RETURNING *", n)
-		} else {
-			c.IndentedJSON(404, gin.H{"message": "book not found"})
+		c.BindJSON(&book)
+
+		err := db.QueryRow("DELETE FROM bookshelf WHERE id=$1 RETRUNING *", book.Id).Scan()
+		if err == nil {
+			c.IndentedJSON(http.StatusOK, book)
+			return
 		}
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "book not found"})
 	}
 }
 
